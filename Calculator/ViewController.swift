8d0f5ac02eeca22b563c92105f0ea1c06c5908d7
Joachim Neumann <joachim@visamedit.com>
2016-05-28 20:20:24 +0200

    nicer symbols on keys

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -71,6 +71,74 @@ class ViewController: UIViewController {
             screenHeight = temp
         }
         displayDescription.hidden = true
+        
+        
+        for stack in keysStack.subviews {
+            for key in stack.subviews {
+                if let b = key as? UIButton {
+                    if let titleLabel = b.titleLabel {
+                        if let titleText = titleLabel.text {
+                            switch titleText {
+                            case "1/x":
+                                let image = UIImage(named: "1_x")
+                                b.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+                                b.setImage(image, forState: UIControlState.Normal)
+                            case "±":
+                                let image = UIImage(named: "plus_minus")
+                                b.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+                                b.setImage(image, forState: UIControlState.Normal)
+                            default: ()
+                            }
+                        }
+                    }
+                }
+            }
+            for stack in scienceStack.subviews {
+                for key in stack.subviews {
+                    if let b = key as? UIButton {
+                        if let titleLabel = b.titleLabel {
+                            if let titleText = titleLabel.text {
+                                switch titleText {
+                                case "x^2":
+                                    let image = UIImage(named: "x_2")
+                                    b.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+                                    b.setImage(image, forState: UIControlState.Normal)
+                                case "x^3":
+                                    let image = UIImage(named: "x_3")
+                                    b.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+                                    b.setImage(image, forState: UIControlState.Normal)
+                                case "x^y":
+                                    let image = UIImage(named: "x_y")
+                                    b.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+                                    b.setImage(image, forState: UIControlState.Normal)
+                                case "e^x":
+                                    let image = UIImage(named: "e_x")
+                                    b.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+                                    b.setImage(image, forState: UIControlState.Normal)
+                                case "10^x":
+                                    let image = UIImage(named: "10_x")
+                                    b.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+                                    b.setImage(image, forState: UIControlState.Normal)
+                                case "√":
+                                    let image = UIImage(named: "sqrt")
+                                    b.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+                                    b.setImage(image, forState: UIControlState.Normal)
+                                case "3√":
+                                    let image = UIImage(named: "sqrt3")
+                                    b.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+                                    b.setImage(image, forState: UIControlState.Normal)
+                                case "x↑↑y":
+                                    let image = UIImage(named: "x_arrow_arrow_x")
+                                    b.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+                                    b.setImage(image, forState: UIControlState.Normal)
+                                default: ()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     func updateDisplay() {
@@ -112,10 +180,24 @@ class ViewController: UIViewController {
     
     func keyFontSize() {
         var buttonFont: UIFont
+        var largeButtonFont: UIFont
         var displayFont: UIFont
-        let buttonFontSize: CGFloat = round(keysStack.bounds.size.height * 0.2 * 0.4)
+        var buttonFontSize: CGFloat
+        var largeButtonFontSize: CGFloat
+        var inset: CGFloat
+        switch UIDevice.currentDevice().orientation{
+        case .LandscapeLeft, .LandscapeRight:
+            buttonFontSize = round(keysStack.bounds.size.height * 0.2 * 0.45)
+            largeButtonFontSize = round(keysStack.bounds.size.height * 0.2 * 0.6)
+            inset = buttonFontSize / 10
+        default: // portrait
+            buttonFontSize = round(keysStack.bounds.size.height * 0.2 * 0.35)
+            largeButtonFontSize = round(keysStack.bounds.size.height * 0.2 * 0.5)
+            inset = buttonFontSize / 3
+        }
         let displayFontSize = min(buttonFontSize, 30)
         buttonFont = UIFont(name: "HelveticaNeue-Thin", size: buttonFontSize)!
+        largeButtonFont = UIFont(name: "HelveticaNeue-Thin", size: largeButtonFontSize)!
         displayFont = UIFont(name: "HelveticaNeue-Thin", size: displayFontSize)!
         for stack in scienceStack.subviews {
             for key in stack.subviews {
@@ -123,20 +205,41 @@ class ViewController: UIViewController {
                     b.titleLabel?.bounds.height
                     b.titleLabel!.font = buttonFont
                     b.titleLabel!.adjustsFontSizeToFitWidth = true
-                    
+                    if let titleLabel = b.titleLabel {
+                        titleLabel.font = buttonFont
+                        if let titleText = titleLabel.text {
+                            switch titleText {
+                            case "x^2", "x^3", "x^y", "e^x", "10^x", "√", "3√", "x↑↑y":
+                                b.imageEdgeInsets = UIEdgeInsetsMake(inset, inset, inset, inset);
+                            default: ()
+                            }
+                        }
+                    }
                 }
             }
         }
         for stack in keysStack.subviews {
             for key in stack.subviews {
                 if let b = key as? UIButton {
-                    b.titleLabel!.font = buttonFont
+                    if let titleLabel = b.titleLabel {
+                        if b.tag == 1 {
+                            titleLabel.font = largeButtonFont
+                        } else {
+                            titleLabel.font = buttonFont
+                        }
+                        if let titleText = titleLabel.text {
+                            switch titleText {
+                            case "1/x", "±":
+                                b.imageEdgeInsets = UIEdgeInsetsMake(inset, inset, inset, inset);
+                            default: ()
+                            }
+                        }
+                    }
                 }
             }
+            display.font = displayFont
         }
-        display.font = displayFont
     }
-    
 
     override func willRotateToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
         layout()
