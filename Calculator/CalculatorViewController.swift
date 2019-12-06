18df21e7f36f80239fbd5aa286c71832b208bd7d
Joachim Neumann <joachim@mindo.io>
2019-12-06 17:41:43 +0700

    better rotation

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -22,12 +22,9 @@ class CalculatorViewController: UIViewController, BrainProtocol {
     @IBOutlet weak var stackView01: UIStackView!
     
     @IBOutlet weak var keysStack: UIStackView!
+    @IBOutlet weak var keysStackWidth: NSLayoutConstraint!
     @IBOutlet weak var keysStackBottom: NSLayoutConstraint!
     @IBOutlet weak var keysStackTrailing: NSLayoutConstraint!
-
-    // strong, because I set isActive = false
-    @IBOutlet var keysStackProportialHeight: NSLayoutConstraint!
-    @IBOutlet var keysStackLeading: NSLayoutConstraint!
     
     fileprivate var spacing: CGFloat = 0
     var zoom = false
@@ -35,15 +32,6 @@ class CalculatorViewController: UIViewController, BrainProtocol {
         get {
             if UIDevice.current.orientation == .landscapeRight ||
                 UIDevice.current.orientation == .landscapeLeft {
-                return true
-            }
-            return false
-        }
-    }
-
-    var portrait: Bool {
-        get {
-            if UIDevice.current.orientation == .portrait {
                 return true
             }
             return false
@@ -107,38 +95,30 @@ class CalculatorViewController: UIViewController, BrainProtocol {
         return UIInterfaceOrientationMask.allButUpsideDown
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        NSLog(portrait ? "viewWillAppear portrait: Yes" : "viewWillAppear portrait: No")
-        NSLog(landscape ? "viewWillAppear landscape: Yes" : "viewWillAppear landscape: No")
-
-        Brain.shared.precision = 100000
-        Brain.shared.brainProtocolDelegate = self
-        Brain.shared.reset() // display --> "0"
-                
-        display.textColor = .white
-        display.contentMode = .bottom
-
-
-        
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        getScreenSize()
+        print(fromInterfaceOrientation)
     }
-    
-    override func viewDidLayoutSubviews() {
-        NSLog(portrait  ? "viewDidLayoutSubviews  portrait: Yes" : "viewDidLayoutSubviews  portrait:  No")
-        NSLog(landscape ? "viewDidLayoutSubviews landscape: Yes" : "viewDidLayoutSubviews landscape:  No")
-        if landscape {
-            spacing = view.bounds.size.height * 0.035
-            keysStackLeading.isActive = false
-            keysStackTrailing.constant = spacing * 1.2
-            keysStackProportialHeight.isActive = true
-            keysStackProportialHeight.constant = 0.6
+    func getScreenSize(){
+        let w = UIScreen.main.bounds.width
+        let h = UIScreen.main.bounds.height
+        print("SCREEN RESOLUTION: "+w.description+" x "+h.description)
+        if w > h {
+            // landscape
+            let sidemargin = w - zoomButton.frame.origin.x + 10
+            keysStackTrailing.constant = sidemargin
+            keysStackWidth.constant = 0.58 * h
+            spacing = h * 0.02
+            displayLeft.constant = sidemargin + 10
+            displayRight.constant = sidemargin + 10
+
         } else {
-            spacing = view.bounds.size.width * 0.035
-            keysStackLeading.isActive = true
-            keysStackLeading.constant  = spacing * 1.2
+            // portrait
+            spacing = w * 0.035
             keysStackTrailing.constant = spacing * 1.2
-            keysStackProportialHeight.isActive = false
+            keysStackWidth.constant = w - 2 * keysStackTrailing.constant
+            displayLeft.constant = 2 * spacing
+            displayRight.constant = 2 * spacing
         }
 
         // distance between the keys
@@ -150,39 +130,83 @@ class CalculatorViewController: UIViewController, BrainProtocol {
         }
         stackView02.spacing = spacing
         stackView01.spacing = spacing
-
-        // make sure there is a bit of space at the bottom
-        keysStackBottom.constant = spacing
-
-        displayLeft.constant = 2 * spacing
-        displayRight.constant = 2 * spacing
-
-        if landscape {
-            return
-        }
-
-        displayHeight.constant = view.frame.size.height * 0.1 * 20
-
-
-        // if the keys ar too high, add space to the left and right,
-        // which results in more space for the display
-        if keysStack.frame.size.height / view.frame.size.height > 0.7 {
-            spacing *= 1.1
-            keysStackLeading.constant = spacing * 2
-            keysStackTrailing.constant = spacing * 2
-//            view.layoutIfNeeded()
-            // NSLog("viewDidLayoutSubviews setNeedsLayout")
-        } else {
-            // NSLog("viewDidLayoutSubviews DONE")
-            // if dispay is high enough, move the keys up
-            // NSLog("viewDidLayoutSubviews %f", displayView.frame.size.height)
-            if keysStack.frame.size.height / view.frame.size.height < 0.6 {
-                keysStackBottom.constant = view.frame.size.height * 0.075
-            }
-        }
-        let fontSize = keysStack.frame.size.height * 0.18
-        display.font = UIFont.systemFont(ofSize: fontSize, weight: .thin)
+        
+        let fontSize = keysStackWidth.constant * 0.2
+        display.font = UIFont.monospacedSystemFont(ofSize: fontSize, weight: .thin)
+        largeDisplay.font = UIFont.monospacedSystemFont(ofSize: fontSize/4, weight: .regular)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        Brain.shared.precision = 100000
+        Brain.shared.brainProtocolDelegate = self
+        Brain.shared.reset() // display --> "0"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        getScreenSize()
+    }
+    
+//    override func viewDidLayoutSubviews() {
+//        return
+//        NSLog(portrait  ? "viewDidLayoutSubviews  portrait: Yes" : "viewDidLayoutSubviews  portrait:  No")
+//        NSLog(landscape ? "viewDidLayoutSubviews landscape: Yes" : "viewDidLayoutSubviews landscape:  No")
+//        if landscape {
+//            spacing = view.bounds.size.height * 0.035
+//            keysStackLeading.isActive = false
+//            keysStackTrailing.constant = spacing * 1.2
+////            keysStackProportialHeight.isActive = true
+////            keysStackProportialHeight.constant = 0.6
+//        } else {
+//            spacing = view.bounds.size.width * 0.035
+//            keysStackLeading.isActive = true
+//            keysStackLeading.constant  = spacing * 1.2
+//            keysStackTrailing.constant = spacing * 1.2
+////            keysStackProportialHeight.isActive = false
+//        }
+//
+//        // distance between the keys
+//        keysStack.spacing = spacing
+//        for v in keysStack.subviews {
+//            if let stack = v as? UIStackView {
+//                stack.spacing = spacing
+//            }
+//        }
+//        stackView02.spacing = spacing
+//        stackView01.spacing = spacing
+//
+//        // make sure there is a bit of space at the bottom
+//        keysStackBottom.constant = spacing
+//
+//        displayLeft.constant = 2 * spacing
+//        displayRight.constant = 2 * spacing
+//
+//        if landscape {
+//            return
+//        }
+//
+//        displayHeight.constant = view.frame.size.height * 0.1 * 20
+//
+//
+//        // if the keys ar too high, add space to the left and right,
+//        // which results in more space for the display
+//        if keysStack.frame.size.height / view.frame.size.height > 0.7 {
+//            spacing *= 1.1
+//            keysStackLeading.constant = spacing * 2
+//            keysStackTrailing.constant = spacing * 2
+////            view.layoutIfNeeded()
+//            // NSLog("viewDidLayoutSubviews setNeedsLayout")
+//        } else {
+//            // NSLog("viewDidLayoutSubviews DONE")
+//            // if dispay is high enough, move the keys up
+//            // NSLog("viewDidLayoutSubviews %f", displayView.frame.size.height)
+//            if keysStack.frame.size.height / view.frame.size.height < 0.6 {
+//                keysStackBottom.constant = view.frame.size.height * 0.075
+//            }
+//        }
+//    }
 
     override var prefersStatusBarHidden: Bool {
         return true
