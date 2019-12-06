f1a7ae4d9f3024cc5b31285af458d0829a4c7560
Joachim Neumann <joachim@mindo.io>
2019-12-06 16:31:01 +0700

    rotation starts to work

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -12,24 +12,44 @@ class CalculatorViewController: UIViewController, BrainProtocol {
     
     @IBOutlet weak var zoomButton: UIButton!
     @IBOutlet weak var copyButton: UIButton!
-    @IBOutlet weak var verticalStack: UIStackView!
     @IBOutlet weak var display: UILabel!
     @IBOutlet weak var largeDisplay: UITextView!
     @IBOutlet weak var displayLeft: NSLayoutConstraint!
     @IBOutlet weak var displayRight: NSLayoutConstraint!
-    @IBOutlet weak var displayBottom: NSLayoutConstraint!
     @IBOutlet weak var displayHeight: NSLayoutConstraint!
 
     @IBOutlet weak var stackView02: UIStackView!
     @IBOutlet weak var stackView01: UIStackView!
     
-    @IBOutlet weak var verticalStackBottom: NSLayoutConstraint!
-    @IBOutlet weak var verticalStackLeading: NSLayoutConstraint!
-    @IBOutlet weak var verticalStackTrailing: NSLayoutConstraint!
+    @IBOutlet weak var keysStack: UIStackView!
+    @IBOutlet weak var keysStackBottom: NSLayoutConstraint!
+    @IBOutlet weak var keysStackTrailing: NSLayoutConstraint!
+
+    // strong, because I set isActive = false
+    @IBOutlet var keysStackProportialHeight: NSLayoutConstraint!
+    @IBOutlet var keysStackLeading: NSLayoutConstraint!
     
     fileprivate var spacing: CGFloat = 0
     var zoom = false
-    
+    var landscape: Bool {
+        get {
+            if UIDevice.current.orientation == .landscapeRight ||
+                UIDevice.current.orientation == .landscapeLeft {
+                return true
+            }
+            return false
+        }
+    }
+
+    var portrait: Bool {
+        get {
+            if UIDevice.current.orientation == .portrait {
+                return true
+            }
+            return false
+        }
+    }
+
     func pendingOperator(name: String) {
         if let op = potentiallyPending[name] {
             op.b.backgroundColor = op.highlightedBackgroundColor
@@ -52,7 +72,7 @@ class CalculatorViewController: UIViewController, BrainProtocol {
     @IBAction func zoomPressed(_ sender: Any) {
         if zoom {
             zoomButton.setImage(UIImage(named: "zoom_out"), for: .normal)
-            verticalStack.isHidden = false
+            keysStack.isHidden = false
             display.isHidden = false
             copyButton.isHidden = true
             largeDisplay.isHidden = true
@@ -61,7 +81,7 @@ class CalculatorViewController: UIViewController, BrainProtocol {
             copyButton.isHidden = false
             largeDisplay.text = Brain.shared.longString()
             largeDisplay.isHidden = false
-            verticalStack.isHidden = true
+            keysStack.isHidden = true
             display.isHidden = true
         }
         zoom = !zoom
@@ -87,62 +107,80 @@ class CalculatorViewController: UIViewController, BrainProtocol {
         return UIInterfaceOrientationMask.allButUpsideDown
     }
 
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        NSLog(portrait ? "viewWillAppear portrait: Yes" : "viewWillAppear portrait: No")
+        NSLog(landscape ? "viewWillAppear landscape: Yes" : "viewWillAppear landscape: No")
 
         Brain.shared.precision = 100000
         Brain.shared.brainProtocolDelegate = self
         Brain.shared.reset() // display --> "0"
                 
-        spacing = view.bounds.size.width * 0.035
+        display.textColor = .white
+        display.contentMode = .bottom
+
+
         
-        // make sure there is a bit of space to the left and right of the keys
-        if verticalStack.frame.origin.x < spacing * 1.2 {
-            verticalStackLeading.constant  = spacing * 1.2
-            verticalStackTrailing.constant = spacing * 1.2
+    }
+    
+    override func viewDidLayoutSubviews() {
+        NSLog(portrait  ? "viewDidLayoutSubviews  portrait: Yes" : "viewDidLayoutSubviews  portrait:  No")
+        NSLog(landscape ? "viewDidLayoutSubviews landscape: Yes" : "viewDidLayoutSubviews landscape:  No")
+        if landscape {
+            spacing = view.bounds.size.height * 0.035
+            keysStackLeading.isActive = false
+            keysStackTrailing.constant = spacing * 1.2
+            keysStackProportialHeight.isActive = true
+            keysStackProportialHeight.constant = 0.6
+        } else {
+            spacing = view.bounds.size.width * 0.035
+            keysStackLeading.isActive = true
+            keysStackLeading.constant  = spacing * 1.2
+            keysStackTrailing.constant = spacing * 1.2
+            keysStackProportialHeight.isActive = false
         }
-        // make sure there is a bit of space at the bottom
-        verticalStackBottom.constant = spacing
-//        if view.frame.size.height - verticalStack.frame.origin.y - verticalStack.frame.size.height < spacing {
-//            verticalStackBottom.constant = spacing
-//        }
-        
-        verticalStack.spacing = spacing
-        for v in verticalStack.subviews {
+
+        // distance between the keys
+        keysStack.spacing = spacing
+        for v in keysStack.subviews {
             if let stack = v as? UIStackView {
                 stack.spacing = spacing
             }
         }
         stackView02.spacing = spacing
         stackView01.spacing = spacing
-        
+
+        // make sure there is a bit of space at the bottom
+        keysStackBottom.constant = spacing
+
         displayLeft.constant = 2 * spacing
         displayRight.constant = 2 * spacing
-        displayBottom.constant = 0
+
+        if landscape {
+            return
+        }
+
         displayHeight.constant = view.frame.size.height * 0.1 * 20
-        display.textColor = .white
-        display.contentMode = .bottom
-    }
-    
-    override func viewDidLayoutSubviews() {
+
+
         // if the keys ar too high, add space to the left and right,
         // which results in more space for the display
-        if verticalStack.frame.size.height / view.frame.size.height > 0.7 {
+        if keysStack.frame.size.height / view.frame.size.height > 0.7 {
             spacing *= 1.1
-            verticalStackLeading.constant = spacing * 2
-            verticalStackTrailing.constant = spacing * 2
-            view.layoutIfNeeded()
+            keysStackLeading.constant = spacing * 2
+            keysStackTrailing.constant = spacing * 2
+//            view.layoutIfNeeded()
             // NSLog("viewDidLayoutSubviews setNeedsLayout")
         } else {
             // NSLog("viewDidLayoutSubviews DONE")
             // if dispay is high enough, move the keys up
             // NSLog("viewDidLayoutSubviews %f", displayView.frame.size.height)
-            if verticalStack.frame.size.height / view.frame.size.height < 0.6 {
-                verticalStackBottom.constant = view.frame.size.height * 0.075
+            if keysStack.frame.size.height / view.frame.size.height < 0.6 {
+                keysStackBottom.constant = view.frame.size.height * 0.075
             }
         }
-        let fontSize = verticalStack.frame.size.height * 0.18
+        let fontSize = keysStack.frame.size.height * 0.18
         display.font = UIFont.systemFont(ofSize: fontSize, weight: .thin)
     }
 
